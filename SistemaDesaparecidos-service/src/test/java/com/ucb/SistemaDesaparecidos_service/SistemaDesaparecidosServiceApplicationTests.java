9fdb1ebfd55981f219777a3c29ca5a55d0c1bb39
AndrePruden <andrepruden0@gmail.com>
2025-03-06 16:14:16 -0400

    first commit

---
+++ SistemaDesaparecidos-service/src/test/java/com/ucb/SistemaDesaparecidos_service/SistemaDesaparecidosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucb.SistemaDesaparecidos_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDesaparecidosServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
